60d49772bd4def82c307d31014e4c61532f52f56
Trip1701 <bj2336@email.vccs.edu>
2019-02-09 12:33:47 -0500

    Reducer Set Up

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -14,10 +14,26 @@
  }
 */
 
+const initialState = {
+  smurfs: [],
+  getSmurfs: false,
+  postSmurfs: false,
+  putSmurfs: false,
+  deleteSmurg: false,
+  error: ''
+}
+
 /*
   You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
+  Feel free to export it as a default and import as rootReducer.
   This will guard your namespacing issues.
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
